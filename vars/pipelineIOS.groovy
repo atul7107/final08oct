49880ddcb072fc2bb43e813fd8cb30352108c753
atul7107 <atul.yadav7717@gmail.com>
2018-10-11 14:26:37 +0530

    Update pipelineIOS.groovy

--- vars/pipelineIOS.groovy
+++ vars/pipelineIOS.groovy
@@ -1,57 +1,69 @@
-#!/usr/bin/groovy
- 
-def call(body) {
-    // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
- 
-    def agent = config.agent ?: 'ios'
- 
- 
-    // now build, based on the configuration provided
-    node(agent) {
- 
-    def workspace = pwd()
- 
-    def pipeline = new cz.ackee.Pipeline()
- 
-    config.pipelineType = 'ios'
- 
-    println "pipeline config from Jenkinsfile ==> ${config}"
- 
-    pipeline.setEnv(config)
-    println "flattened pipeline config ==> ${config}"
- 
-    env.FASTLANE_SKIP_UPDATE_CHECK = 1
-    env.FASTLANE_DISABLE_COLORS = 1
-    env.CHANGELOG_PATH = "outputs/changelog.txt"
-    def slackChannel = config.slackChannel ?: 'ci-ios' // don't care if it exists
- 
-    def reason = 'start'
- 
-    try {
- 
-        properties([
-            disableConcurrentBuilds(),
-        ])
- 
-        stage('Checkout') {
-            reason = 'checkout'
-            // Checkout code from repository and update any submodules
-            pipeline.checkoutScm()
-            sh "mkdir -p outputs"
- 
-            // generate changelog
-            pipeline.generateChangelog path: env.CHANGELOG_PATH, format: 'format:- %s [%ce]'
+#!groovy
+
+pipeline {
+  agent any
+
+  stages {
+    stage('Checkout') {
+      steps {
+        checkout scm
+      }
+    }
+
+    stage('increment') {
+      steps {
+        sh 'fastlane feature'
+      }
+    }
+
+    stage('build') {
+      steps {
+        sh 'fastlane build'
+       }
+     } 
+
+    stage('unit test') {
+      when {
+        expression {
+          env.BRANCH_NAME == 'develop'
         }
-      currentBuild.result = 'SUCCESS'
-    } catch (e) {
-        currentBuild.result = 'FAILURE'
-        throw e
-    } finally {
-        pipeline.notifySlack(channel: slackChannel, reason: reason)
+      }
+      steps {
+        // Generating docs
+        sh 'fastlane unittest'
+      }
     }
-    }
+  }
+
+
+// Slack notification with status and code changes from git
+def notifyBuild(String buildStatus = 'SUCCESSFUL') {
+  buildStatus = buildStatus
+
+  def colorName = 'RED'
+  def colorCode = '#FF0000'
+  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+  def changeSet = getChangeSet()
+  def message = "${subject} \n ${changeSet}"
+
+  if (buildStatus == 'SUCCESSFUL') {
+    color = 'GREEN'
+    colorCode = '#00FF00'
+  } else {
+    color = 'RED'
+    colorCode = '#FF0000'
+  }
+
+  slackSend (color: colorCode, message: message)
+}
+
+@NonCPS
+
+// Fetching change set from Git
+def getChangeSet() {
+  return currentBuild.changeSets.collect { cs ->
+    cs.collect { entry ->
+        "* ${entry.author.fullName}: ${entry.msg}"
+    }.join("\n")
+  }.join("\n")
 }
