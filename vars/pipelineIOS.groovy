96ac9d95b2f791850f792ff2d09875404152a631
atul7107 <atul.yadav7717@gmail.com>
2018-10-11 14:28:24 +0530

    Update pipelineIOS.groovy

--- vars/pipelineIOS.groovy
+++ vars/pipelineIOS.groovy
@@ -34,36 +34,3 @@ pipeline {
       }
     }
   }
-
-
-// Slack notification with status and code changes from git
-def notifyBuild(String buildStatus = 'SUCCESSFUL') {
-  buildStatus = buildStatus
-
-  def colorName = 'RED'
-  def colorCode = '#FF0000'
-  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-  def changeSet = getChangeSet()
-  def message = "${subject} \n ${changeSet}"
-
-  if (buildStatus == 'SUCCESSFUL') {
-    color = 'GREEN'
-    colorCode = '#00FF00'
-  } else {
-    color = 'RED'
-    colorCode = '#FF0000'
-  }
-
-  slackSend (color: colorCode, message: message)
-}
-
-@NonCPS
-
-// Fetching change set from Git
-def getChangeSet() {
-  return currentBuild.changeSets.collect { cs ->
-    cs.collect { entry ->
-        "* ${entry.author.fullName}: ${entry.msg}"
-    }.join("\n")
-  }.join("\n")
-}
